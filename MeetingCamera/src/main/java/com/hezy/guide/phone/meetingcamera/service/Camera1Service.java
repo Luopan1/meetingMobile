e745f7e20fc50eadc4ab9a4ab02e1cb10565f7c6
Darkbug <896505009@qq.com>
2018-12-15 15:34:07 +0800

    OSG-379,OSG-467: 1.修复参会人图像压缩问题，mobile端、tv端参会人图像上传时，进行图像压缩后再上传。

--- MeetingCamera/src/main/java/com/hezy/guide/phone/meetingcamera/service/Camera1Service.java
+++ MeetingCamera/src/main/java/com/hezy/guide/phone/meetingcamera/service/Camera1Service.java
@@ -2,6 +2,8 @@ package com.hezy.guide.phone.meetingcamera.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Binder;
@@ -11,6 +13,7 @@ import android.view.SurfaceHolder;
 
 import com.hezy.guide.phone.meetingcamera.camera.CameraHelper;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -155,7 +158,7 @@ public class Camera1Service extends Service {
                                 new Thread(new Runnable() {
                                     @Override
                                     public void run() {
-                                        savePicture(data);
+                                        savePicture(compressImage(data));
                                     }
                                 }).start();
                             } else {
@@ -170,11 +173,35 @@ public class Camera1Service extends Service {
         });
     }
 
+    private byte[] compressImage(byte[] fileBytes) {
+        Log.i(TAG, "参会人图像压缩前大小：" + fileBytes.length);
+        //压缩图片
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        options.inSampleSize = 8;
+        options.inJustDecodeBounds = false;
+        Bitmap afterBitmap = BitmapFactory.decodeByteArray(fileBytes, 0, fileBytes.length, options);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        afterBitmap.compress(Bitmap.CompressFormat.PNG, 70, baos);
+        byte[] bytes = baos.toByteArray();
+        Log.i(TAG, "参会人图像压缩后大小：" + bytes.length);
+
+        // 先判断是否已经回收
+        if (afterBitmap != null && !afterBitmap.isRecycled()) {
+            // 回收并且置为null
+            afterBitmap.recycle();
+            afterBitmap = null;
+        }
+        System.gc();
+
+        return bytes;
+    }
 
     private Camera.PictureCallback pictureJPEGCallback = new Camera.PictureCallback() {
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
-            savePicture(data);
+            savePicture(compressImage(data));
         }
     };
 
